ef1e291f8399c5a0586740a169297ff100a1ae1e
Daniel Barnes <dabarnes2b@gmail.com>
2022-09-19 11:46:24 -0500

    hack build script for mac / clang cpp

--- build.sh
+++ build.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 #shopt -s nullglob
-echo "Purging existing workflows..."
-rm .github/workflows/*
-for workflow in workflows/*
-do
+
+is_mac=$(uname)
+
+build() {
+  if [ "$IN_DOCKER" == "1" ]; then
+    echo "IN CONTAINER"
+    pushd /opt/playground
+  fi
+
+  echo "Purging existing workflows..."
+  rm .github/workflows/*
+  for workflow in workflows/*
+  do
     echo "Processing $workflow > .github/workflows/$workflow"
     #cpp -nostdinc -P $workflow | sed '/./,$!d' > .github/$workflow
     cpp -nostdinc -w -P $workflow | sed '/./,$!d' > .github/$workflow
-done
+  done
+}
+
+
+if [ "$is_mac" == "Darwin" ]; then
+  echo "Running with container"
+  docker run --rm \
+    -v "$(git rev-parse --show-toplevel)":/opt/playground \
+    ghcr.io/iterative/cml \
+    /opt/playground/build.sh
+  exit
+fi
+build
